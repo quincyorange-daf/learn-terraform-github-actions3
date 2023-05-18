e8895b7a261060808f613bfd45da41156460efb2
quincyorange-daf <132629787+quincyorange-daf@users.noreply.github.com>
2023-05-18 15:09:41 +1000

    name  var add
    name  var add

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-
+  tags = {var.instance_name}
   user_data = <<-EOF
               #!/bin/bash
               apt-get update
